381bb9c533337ba54c74acce93ec766db829788c
MTillart <merliti.tillart@khk.ee>
2019-03-07 11:13:59 +0200

    18nes peatükk läbitud. Kõik testid said rohelise tule

--- add.js
+++ add.js
@@ -15,3 +15,10 @@ result =  x + y;
 
 //Expect add(0.1, 0.2) to equal 0.3
 expect(add(0.1, 0.2)).toBe(0.3);
+
+expect(add(0.1, 0.2)).not.toBe(0.1);
+
+it("should consider this spec", function() {
+    expect(true).toBe(true);
+    expect(false).not.toBe(false);
+});
